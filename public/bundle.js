eeb68086beb1100713b4d4d693cef4068578cfd2
Richard Silverton <silverton.richard@googlemail.com>
2016-02-12 09:08:05 +0000

    Added second map

--- public/bundle.js
+++ public/bundle.js
@@ -1,41 +1,53 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 if ($('body.locations').length) {
   require('./lib/Leaflet.MakiMarkers.js')
-  setupTuscanyMap()
+  setupMaps()
+}
 
-  function setupTuscanyMap () {
-    var map = L.map('tuscany-map').setView([51.505, -0.09], 13);
+function setupMaps () {
+  var tuscanyMap = L.map('tuscany-map').setView([43.2580769, 11.6180715], 13)
+  var serreMap = L.map('serre-map').setView([43.2580769, 11.6180715], 15)
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(tuscanyMap)
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(serreMap)
 
-    var castelloIcon = L.MakiMarkers.icon({icon: 'star', color: '#ee9988', size: 'l'})
-    var airportIcon = L.MakiMarkers.icon({icon: 'airport', color: '#998844', size: 'l'})
+  var castelloIcon = L.MakiMarkers.icon({icon: 'star', color: '#ee9988', size: 'l'})
+  var airportIcon = L.MakiMarkers.icon({icon: 'airport', color: '#998844', size: 'l'})
+  var townHallIcon = L.MakiMarkers.icon({icon: 'town-hall', color: '#ee9988', size: 'l'})
+  var trainIcon = L.MakiMarkers.icon({icon: 'rail', color: '#998844', size: 'l'})
 
-    var markers = []
-    markers.push(L.marker([43.2580769, 11.6180715], {icon: castelloIcon}).addTo(map)
-      .bindPopup('Castello delle Serre').openPopup())
-    // markers.push(L.marker([43.3314949, 11.7252489]).addTo(map)
-    //   .bindPopup('Town Hall, Monte San Savino'))
-    // markers.push(L.marker([43.2832859,11.6025952]).addTo(map)
-    //   .bindPopup('Rapelano Terme train station'))
-    markers.push(L.marker([43.0947870,12.5033100], {icon: airportIcon}).addTo(map)
-      .bindPopup('Perugia airport'))
-    markers.push(L.marker([43.8086530,11.2012250], {icon: airportIcon}).addTo(map)
-      .bindPopup('Florence airport'))
-    markers.push(L.marker([43.6890840,10.3978850], {icon: airportIcon}).addTo(map)
-      .bindPopup('Pisa airport'))
-    markers.push(L.marker([44.5345220,11.2878590], {icon: airportIcon}).addTo(map)
-      .bindPopup('Bologna airport'))
-    markers.push(L.marker([41.7993350,12.5903280], {icon: airportIcon}).addTo(map)
-      .bindPopup('Rome airport (Ciampino)'))
-    markers.push(L.marker([41.7998870,12.2462380], {icon: airportIcon}).addTo(map)
-      .bindPopup('Rome airport (Leonardo da Vinci)'))
+  var tuscanyMarkers = []
+  var serreMarkers = []
+  tuscanyMarkers.push(L.marker([43.2580769, 11.6180715], {icon: castelloIcon}).addTo(tuscanyMap)
+    .bindPopup('Castello delle Serre').openPopup())
+  tuscanyMarkers.push(L.marker([43.0947870,12.5033100], {icon: airportIcon}).addTo(tuscanyMap)
+    .bindPopup('Perugia airport'))
+  tuscanyMarkers.push(L.marker([43.8086530,11.2012250], {icon: airportIcon}).addTo(tuscanyMap)
+    .bindPopup('Florence airport'))
+  tuscanyMarkers.push(L.marker([43.6890840,10.3978850], {icon: airportIcon}).addTo(tuscanyMap)
+    .bindPopup('Pisa airport'))
+  tuscanyMarkers.push(L.marker([44.5345220,11.2878590], {icon: airportIcon}).addTo(tuscanyMap)
+    .bindPopup('Bologna airport'))
+  tuscanyMarkers.push(L.marker([41.7993350,12.5903280], {icon: airportIcon}).addTo(tuscanyMap)
+    .bindPopup('Rome airport (Ciampino)'))
+  tuscanyMarkers.push(L.marker([41.7998870,12.2462380], {icon: airportIcon}).addTo(tuscanyMap)
+    .bindPopup('Rome airport (Leonardo da Vinci)'))
+  serreMarkers.push(L.marker([43.2580769, 11.6180715], {icon: castelloIcon}).addTo(serreMap)
+    .bindPopup('Castello delle Serre').openPopup())
+  serreMarkers.push(L.marker([43.3314949, 11.7252489], {icon: townHallIcon}).addTo(serreMap)
+    .bindPopup('Town Hall, Monte San Savino'))
+  serreMarkers.push(L.marker([43.2832859,11.6025952], {icon: trainIcon}).addTo(serreMap)
+    .bindPopup('Rapelano Terme train station'))
 
-    var markerGroup = new L.featureGroup(markers)
-    map.fitBounds(markerGroup.getBounds())
-  }
+  var tuscanyMarkerGroup = new L.featureGroup(tuscanyMarkers)
+  tuscanyMap.fitBounds(tuscanyMarkerGroup.getBounds())
+  var serreMarkerGroup = new L.featureGroup(serreMarkers)
+  serreMap.fitBounds(serreMarkerGroup.getBounds())
+
 }
 
 },{"./lib/Leaflet.MakiMarkers.js":2}],2:[function(require,module,exports){
